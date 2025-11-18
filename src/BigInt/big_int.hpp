4c5fc09ef0cc03d353b5ed69095de2727d55cbae
neil <neilien@icloud.com>
2025-11-18 17:43:02 +0100

    petits ajouts -a

--- src/BigInt/big_int.hpp
+++ src/BigInt/big_int.hpp
@@ -1,29 +1,25 @@
-// big_int_libs.hpp
+// big_int.hpp
+#pragma once
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
-
 #include <string>
-
-
 #include <vector>
 #include <array>
-
-
 #include <map>
 #include <unordered_map>
 #include <set>
 #include <unordered_set>
-
 #include <algorithm>
 #include <numeric>
-
-
 #include <cmath>
 #include <cstdlib>
 
+// Boost pour les big integers et floats
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/cpp_dec_float.hpp>
+
+// GMP pour les big integers
+#include <gmp.h>
 
